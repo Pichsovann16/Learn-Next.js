6f500e8e50657a974d6af75752c9c143c5bb9b1d
{Pichsovann16e} <{pichsovann16@gmail.com}>
2025-10-21 12:48:26 +0700

    update thumnail with file base metadata

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -4,15 +4,18 @@ import { NavbarComponent } from "@/components/navbar/NavbarComponent";
 import Loading from "./loading";
 import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 import ErrorComponent from "./globalerror";
-import {inter, suwannaphum, localCustomFont} from "./font";
+import { inter, suwannaphum, localCustomFont } from "./font";
+import { Metadata } from "next";
 
-// export const metadata: Metadata = {
-//   title: "E-Commerce App",
-//   description: "An e-commerce application built with Next.js",
-//   // openGraph: {
-//   //   images: ['/vercel.svg'],
-//   // }
-// }
+export const metadata: Metadata = {
+  title: "E-Commerce App",
+  description: "An e-commerce application built with Next.js",
+  openGraph: {
+    title: "E-Commerce App",
+    description: "An e-commerce application built with Next.js",
+    images: ["/opengraph-image.png"],
+  },
+};
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -20,7 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className = {`${inter.variable} ${suwannaphum.variable} ${localCustomFont.variable}`}>
+      <body
+        className={`${inter.variable} ${suwannaphum.variable} ${localCustomFont.variable}`}
+      >
         <header>
           <NavbarComponent />
         </header>
